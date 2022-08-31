d8d754c7abf94c0cada9fa04b7ec2e48e0cb8233
Mark - DopestGeek <markmayalla@gmail.com>
2022-08-31 13:25:30 +0300

    implemented routes for web and api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Config\UnitTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware("auth:sanctum")->name("api.")->group(function () {
+    
+    Route::prefix("/config")->name("config.")->group(function () {
+        Route::resource("unitTypes", UnitTypeController::class);
+    });
 });
