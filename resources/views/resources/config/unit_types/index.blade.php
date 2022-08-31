f586e377c651a90b6d1129048c9590e7bff3bb6f
Mark - DopestGeek <markmayalla@gmail.com>
2022-08-31 13:31:33 +0300

    implemented index page

---
+++ resources/views/resources/config/unit_types/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Unit Types') }}</div>
+
+                    <div class="card-body">
+                       
+
+                        <a href="{{ route("config.unitTypes.create") }}" class="btn btn-primary">Add</a>
+
+                        <table class="table table-stripped">
+                            <thead>
+                                <tr>
+                                    <th>S/n</th>
+                                    <th>Name</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @foreach ($unitTypes as $unitType)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $unitType->name }}</td>
+                                        <td>
+                                            <a href="{{ route("config.userTypes.edit", ["unitType" => $unitType]) }}" class="btn btn-info">Edit</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
