e2fc32e173c06f63c1780dcb2de4ad6c39cf826a
Mark - DopestGeek <markmayalla@gmail.com>
2022-09-02 19:53:45 +0300

    implemented a model for InventoryWarehouse

--- app/Models/Inventory/InventoryWarehouse.php
+++ app/Models/Inventory/InventoryWarehouse.php
@@ -2,10 +2,20 @@
 
 namespace App\Models\Inventory;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class InventoryWarehouse extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ["name", "description", "featured_image"];
+
+    protected function featuredImage(): Attribute {
+        return Attribute::make(fn ($val) => Storage::url($val));
+    }
 }
