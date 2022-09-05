06f014fda99fe2aab19fdafc35063ce29d4a0110
Mark - DopestGeek <markmayalla@gmail.com>
2022-09-05 14:44:03 +0300

    continue writting tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public $user;
+
+    protected function setUp(): void {
+        parent::setUp();
+    }
+
+    public function makeUserAndSignIn() {
+        $user = User::factory()->create();
+        $this->user = $user;
+        $this->actingAs($user);
+        return $this;
+    }
 }
