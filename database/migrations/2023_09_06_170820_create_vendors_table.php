b4a5aa12d6ee6b1121a9a54a7cb27c16a0e8693f
Kawogo <dkawogo79@gmail.com>
2023-09-17 21:15:14 +0300

    changes from production

--- database/migrations/2023_09_06_170820_create_vendors_table.php
+++ database/migrations/2023_09_06_170820_create_vendors_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('vendors', function (Blueprint $table) {
             $table->id();
             $table->string("name")->unique();
+            $table->string("type");
+            $table->string("phone");
+            $table->string("email");
             $table->string("description")->nullable();
             $table->timestamps();
         });
