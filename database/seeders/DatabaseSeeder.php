e72832f0a2e8389a2ba9911b572c84134eff5598
kawogo <dkawogo79@gmail.com>
2024-06-18 20:51:14 +0300

    Login slider and logo...

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            InventoryWarehouseSeeder::class,
             UserSeeder::class,
             InventoryCategorySeeder::class,
             //  InventoryItemsSeender::class,
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,73 +39,114 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
+        .full-height {
+            height: 100%;
+        }
+        .carousel-item img {
+            width: 100%;
+            height: auto;
+            max-height: 100%;
+            object-fit: cover;
+        }
+    </style>
 </head>
 <body>
-    <div class="app app-auth-sign-in align-content-stretch d-flex flex-wrap justify-content-end">
-    
-            <div class="app-auth-background">
-
-            </div>
-            <div class="app-auth-container">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    {{-- <div class="logo">
-                        <a href="index.html">Photon</a>
-                    </div> --}}
-                    {{-- <p class="auth-description">Please sign-in to your account and continue to the dashboard.<br>Don't have an account? <a href="sign-up.html">Sign Up</a></p> --}}
-<div class="mb-4">
-    <h4 class="text-center">LOGIN</h4>
-</div>
-                    <div class="auth-credentials m-b-xxl">
-
-                                    
-                        <div class="form-group">
-                            <label for="signInEmail" class="form-label">Email address</label>
-                            <input type="email" name="email" class="form-control m-b-md @error('email') is-invalid @enderror" id="signInEmail" aria-describedby="signInEmail" placeholder="user@photon.co.tz" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+    <div class="container-fluid full-height d-flex align-items-center justify-content-center">
+        <div class="row w-100 h-100 d-flex align-items-stretch">
+            <div class="col-md-8">
+                <div id="carouselExampleControls" class="carousel slide h-100" data-bs-ride="carousel">
+                    <div class="carousel-inner h-100">
+                        <div class="carousel-item active h-100">
+                            <img src="{{ asset('neptune/images/login/1.jpeg') }}" class="d-block w-100 h-100" alt="...">
                         </div>
-
-                        <div class="form-group">
-                            <label for="signInPassword" class="form-label">Password</label>
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/2.jpeg') }}" class="d-block w-100 h-100" alt="...">
                         </div>
-
-                     
-                        <div class="form-check mt-3">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/3.jpeg') }}" class="d-block w-100 h-100" alt="...">
+                        </div>
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/4.jpeg') }}" class="d-block w-100 h-100" alt="...">
+                        </div>
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/5.jpeg') }}" class="d-block w-100 h-100" alt="...">
+                        </div>
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/6.jpeg') }}" class="d-block w-100 h-100" alt="...">
+                        </div>
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/7.jpeg') }}" class="d-block w-100 h-100" alt="...">
+                        </div>
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/8.jpeg') }}" class="d-block w-100 h-100" alt="...">
+                        </div>
+                        <div class="carousel-item h-100">
+                            <img src="{{ asset('neptune/images/login/9.jpeg') }}" class="d-block w-100 h-100" alt="...">
                         </div>
                     </div>
-
-                    <div class="auth-submit">
-                        <button type="submit" class="btn btn-primary">Sign In</button>
-                        @if (Route::has('password.request'))
-                            <a href="{{ route('password.request') }}" class="auth-forgot-password float-end">{{ __('Forgot Your Password?') }}</a>
-                        @endif
-                        
-                    </div>
-                    <div class="divider"></div>
-                    {{-- <div class="auth-alts">
-                        <a href="#" class="auth-alts-google"></a>
-                        <a href="#" class="auth-alts-facebook"></a>
-                        <a href="#" class="auth-alts-twitter"></a>
-                    </div> --}}
-                </form>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
+            <div class="col-md-4 bg-white d-flex align-items-center">
+                <div class="p-4 w-100">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <center>
+                            <img src="{{ asset('neptune/images/logo.jpeg') }}" alt="" srcset="" width="200" height="100">
+                        </center>
+                        <div class="mb-4 mt-4">
+                            <h4 class="text-center">LOGIN</h4>
+                        </div>
+                        <div class="auth-credentials mb-4">
+                            <div class="form-group mb-3">
+                                <label for="signInEmail" class="form-label">Email address</label>
+                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="signInEmail" placeholder="user@photon.co.tz" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="signInPassword" class="form-label">Password</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-check mt-3">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                        </div>
+                        <div class="auth-submit">
+                            <button type="submit" class="btn btn-primary w-100">Sign In</button>
+                            @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}" class="auth-forgot-password float-end mt-3">{{ __('Forgot Your Password?') }}</a>
+                            @endif
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-
+    
      <!-- Javascripts -->
      <script src="{{ asset('neptune/plugins/jquery/jquery-3.5.1.min.js') }}"></script>
      <script src="{{ asset('neptune/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
